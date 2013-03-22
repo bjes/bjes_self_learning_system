8e32a7a2001ebd4f1a46f6b7c431b881236b08fc
William Wu <william@pylabs.net>
2013-03-22 12:31:17 +0800

    完成 WebView 上方的 Scan 按鈕的功能

--- src/tw/edu/tp/bjes/sls/LearningActivity.java
+++ src/tw/edu/tp/bjes/sls/LearningActivity.java
@@ -2,10 +2,14 @@ package tw.edu.tp.bjes.sls;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
 import android.webkit.WebChromeClient;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class LearningActivity extends Activity {
     private WebView web_view;
@@ -13,7 +17,11 @@ public class LearningActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning);
-        
+
+        // scan button
+        Button btn_scan = (Button) findViewById(R.id.btn_scan);
+        btn_scan.setOnClickListener(startScanner);
+
         Bundle bundle = this.getIntent().getExtras();
         String scan_result = bundle.getString("SCAN_RESULT");
         web_view = (WebView) findViewById(R.id.web_view);
@@ -27,5 +35,24 @@ public class LearningActivity extends Activity {
         web_view.setWebChromeClient(new WebChromeClient());
         web_view.setWebViewClient(new WebViewClient());
         web_view.loadUrl(scan_result);
+    }
+
+    private OnClickListener startScanner = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+            startActivityForResult(intent, 0);
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+                web_view.loadUrl(contents);
+            }
+        }
     }
 }
